b725b1c9f77efa585857e91fe79e1d40a241be63
Vijay Chavre <chavre.vijay@gmail.com>
2024-03-17 17:35:30 +0530

    Added async handler

--- src/controllers/v1/authController.js
+++ src/controllers/v1/authController.js
@@ -1,27 +1,25 @@
+import { asyncHandler } from '../../utils/asyncHandler.js';
 import sendSuccess from '../../utils/sucessHandler.js';
 import jwt from 'jsonwebtoken';
-export const signIn = async (req, res, next) => {
-  try {
-    const user = req.user;
-    //Generate and return JWT token
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' }
-    );
 
-    sendSuccess(
-      res,
-      {
-        token,
-        user: req.user,
-      },
-      200
-    );
-  } catch (err) {
-    next(err);
-  }
-};
+export const signIn = asyncHandler(async (req, res, next) => {
+  const user = req.user;
+  //Generate and return JWT token
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+
+  sendSuccess(
+    res,
+    {
+      token,
+      user: req.user,
+    },
+    200
+  );
+});
