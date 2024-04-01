e56b7fc72ff311f948a03795b1f8314ead330a63
Vijay Chavre <chavre.vijay@gmail.com>
2024-04-02 00:04:05 +0530

    Added refresh token logic

--- src/controllers/v1/User/authController.ts
+++ src/controllers/v1/User/authController.ts
@@ -1,8 +1,11 @@
 import { asyncHandler } from '../../../utils/asyncHandler.ts';
 import sendSuccess from '../../../utils/sucessHandler.ts';
-import jwt from 'jsonwebtoken';
 import { Request, RequestHandler, Response } from 'express';
 import { CustomError } from '../../../utils/errorhandler.ts';
+import {
+  generateTokens,
+  getUserFromToken,
+} from '../../../utils/tokenHandler.ts';
 type User = {
   id: string;
   name: string;
@@ -11,28 +14,6 @@ type User = {
 };
 export interface AuthenticatedRequest extends Request {
   user: User;
-}
-
-/**
- * Generate access token and refresh token
- * @param user The user to generate tokens for
- * @returns An object with the access token and refresh token
- */
-function generateTokens(user: { id: string }): {
-  accessToken: string;
-  refreshToken: string;
-} {
-  if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET not defined');
-  }
-
-  const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-    expiresIn: '15m',
-  } as jwt.SignOptions);
-
-  const refreshToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-
-  return { accessToken, refreshToken };
 }
 
 export const signIn: RequestHandler = asyncHandler(async (req, res) => {
@@ -52,21 +33,6 @@ export const signIn: RequestHandler = asyncHandler(async (req, res) => {
     );
   }
 });
-
-const getUserFromToken = async (token: string): Promise<User> => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET not defined');
-  }
-
-  let user;
-  try {
-    user = jwt.verify(token, process.env.JWT_SECRET) as User;
-  } catch (error) {
-    throw error;
-  }
-
-  return user;
-};
 
 export const refreshToken: RequestHandler = asyncHandler(async (req, res) => {
   const refreshToken = req.body.refreshToken;
---
+++ src/utils/tokenHandler.ts
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+/**
+ * Generate access token and refresh token
+ * @param user The user to generate tokens for
+ * @returns An object with the access token and refresh token
+ */
+export function generateTokens(user: { id: string }): {
+  accessToken: string;
+  refreshToken: string;
+} {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET not defined');
+  }
+
+  const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: '15m',
+  } as jwt.SignOptions);
+
+  const refreshToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+  return { accessToken, refreshToken };
+}
+
+export const getUserFromToken = async (token: string): Promise<User> => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET not defined');
+  }
+
+  let user;
+  try {
+    user = jwt.verify(token, process.env.JWT_SECRET) as User;
+  } catch (error) {
+    throw error;
+  }
+
+  return user;
+};
